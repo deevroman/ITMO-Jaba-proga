431bb8ee1bcb304d2cd7e34374efe5929cdc37f3
deevroman <roman.deev06@gmail.com>
2021-06-19 16:06:35 +0300

    structure refactor

--- lab8_final/src/main/java/Server/src/main/java/ru/trickyfoxy/lab8/example_db_config.java
+++
@@ -1,7 +0,0 @@
-package ru.trickyfoxy.lab8;
-
-public class example_db_config {
-    static final public String uri = "";
-    static final public String user = "";
-    static final public String password = "";
-}
